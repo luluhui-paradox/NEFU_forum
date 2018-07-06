5035921a676fac61c537f48f79523775a945a7f5
wwc <2543816873@qq.com>
2018-07-06 09:54:06 +0800

    修改了路由，添加了一些必要的注释，整理了项目结构

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,40 +1,43 @@
 export default [
+  //注册页面
   {
     path: '/auth/register',
     name: 'Register',
     component: () => import('@/views/auth/Register')
   },
+  //登录页面
+  {
+    path: '/auth/login',
+    name: 'Login',
+    component: () => import('@/views/auth/Login')
+  },
+  //主页
   {
     path: '/',
     name: 'Home',
     alias: '/topics',
     component: () => import('@/views/Home')
   },
-  // {
-  //   path: '*',
-  //   redirect: '/'
-  // },
-  {
-    path: '/auth/login',
-    name: 'Login',
-    component: () => import('@/views/auth/Login')
-  },
+  //个人信息页面
   {
     path: '/users/1/edit',
     component: () => import('@/views/users/Edit.vue'),
     children: [
+      //个人信息
       {
         path: '',
         name: 'EditProfile',
         component: () => import('@/views/users/Profile.vue'),
         meta: { auth: true }
       },
-      {
-        path: '/users/1/edit_avatar',
-        name: 'EditAvatar',
-        component: () => import('@/views/users/Avatar.vue'),
-        meta: { auth: true }
-      },
+      //修改头像，这是后续要加的功能
+      // {
+      //   path: '/users/1/edit_avatar',
+      //   name: 'EditAvatar',
+      //   component: () => import('@/views/users/Avatar.vue'),
+      //   meta: { auth: true }
+      // },
+      //修改密码
       {
         path: '/users/1/edit_password',
         name: 'EditPassword',
@@ -43,27 +46,33 @@ export default [
       }
     ]
   },
+  //帖子模块
+  //创建帖子
   {
     path: '/articles/create',
     name: 'Create',
     component: () => import('@/views/articles/Create'),
     meta: { auth: true }
   },
+  //编辑帖子
   {
     path: '/articles/:articleId/edit',
     name: 'Edit',
     component: () => import('@/views/articles/Create'),
     meta: { auth: true }
   },
+  //用户专栏模块
  {
   path: '/:user',
   component: () => import('@/views/articles/Column'),
     children: [
+      //用户帖子列表
       {
         path: '',
         name: 'Column',
         component: () => import('@/views/articles/List.vue')
       },
+      //用户帖子内容
       {
         path: '/articles/:articleId/content',
         name: 'Content',
--- src/views/users/Edit.vue
+++ src/views/users/Edit.vue
@@ -28,11 +28,11 @@ export default {
           title: '个人信息',
           icon: 'list-alt'
         },
-        {
-          name: 'edit_avatar',
-          title: '修改头像',
-          icon: 'picture-o'
-        },
+        // {
+        //   name: 'edit_avatar',
+        //   title: '修改头像',
+        //   icon: 'picture-o'
+        // },
         {
           name: 'edit_password',
           title: '修改密码',
